6a16152576db358929e8606b3d0b6abe725f724b
proiacm <proiacm@gmail.com>
2020-12-17 11:52:59 -0700

    add Logo to Nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Nav.css';
 import {Link} from 'react-scroll';
+import logo from '../Images/Logo.png';
 
 const Nav = () => {
 
@@ -8,6 +9,7 @@ const Nav = () => {
     <div className='Nav'>
       <Link className='nav-li-left' style={{ cursor: "pointer"}} to="store" spy={true} smooth={true}>Store</Link>
       <Link className='nav-li-left' style={{ cursor: "pointer"}} to="tutorials" spy={true} smooth={true}>Tutorials</Link>
+      <img className='nav-img' src={logo} alt='logo'/>
       <Link className='nav-li-right' style={{ cursor: "pointer"}} to="blog" spy={true} smooth={true}>Blog</Link>
       <Link className='nav-li-right' style={{ cursor: "pointer"}} to="contact" spy={true} smooth={true}>Contact</Link>
     </div>
