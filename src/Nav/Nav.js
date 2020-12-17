2ce22c829ebf1aa45d7192463baa9fe8840cb306
proiacm <proiacm@gmail.com>
2020-12-17 11:47:05 -0700

    Remove underline from nav links

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,10 +6,10 @@ const Nav = () => {
 
   return (
     <div className='Nav'>
-      <Link className='nav-li-left' style={{ cursor: "pointer"}} to="store" spy={true} smooth={true}><u>Store</u></Link>
-      <Link className='nav-li-left' style={{ cursor: "pointer"}} to="tutorials" spy={true} smooth={true}><u>Tutorials</u></Link>
-      <Link className='nav-li-right' style={{ cursor: "pointer"}} to="blog" spy={true} smooth={true}><u>Blog</u></Link>
-      <Link className='nav-li-right' style={{ cursor: "pointer"}} to="contact" spy={true} smooth={true}><u>Contact</u></Link>
+      <Link className='nav-li-left' style={{ cursor: "pointer"}} to="store" spy={true} smooth={true}>Store</Link>
+      <Link className='nav-li-left' style={{ cursor: "pointer"}} to="tutorials" spy={true} smooth={true}>Tutorials</Link>
+      <Link className='nav-li-right' style={{ cursor: "pointer"}} to="blog" spy={true} smooth={true}>Blog</Link>
+      <Link className='nav-li-right' style={{ cursor: "pointer"}} to="contact" spy={true} smooth={true}>Contact</Link>
     </div>
   )
 }
