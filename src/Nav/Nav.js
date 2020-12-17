6a12d9fe06fca36786bbb3d0c2c6bd13b096ae44
proiacm <proiacm@gmail.com>
2020-12-17 11:46:14 -0700

    Space nav links

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,10 +6,10 @@ const Nav = () => {
 
   return (
     <div className='Nav'>
-      <Link style={{ cursor: "pointer"}} to="store" spy={true} smooth={true}><u>Store</u></Link>
-      <Link style={{ cursor: "pointer"}} to="tutorials" spy={true} smooth={true}><u>Tutorials</u></Link>
-      <Link style={{ cursor: "pointer"}} to="blog" spy={true} smooth={true}><u>Blog</u></Link>
-      <Link style={{ cursor: "pointer"}} to="contact" spy={true} smooth={true}><u>Contact</u></Link>
+      <Link className='nav-li-left' style={{ cursor: "pointer"}} to="store" spy={true} smooth={true}><u>Store</u></Link>
+      <Link className='nav-li-left' style={{ cursor: "pointer"}} to="tutorials" spy={true} smooth={true}><u>Tutorials</u></Link>
+      <Link className='nav-li-right' style={{ cursor: "pointer"}} to="blog" spy={true} smooth={true}><u>Blog</u></Link>
+      <Link className='nav-li-right' style={{ cursor: "pointer"}} to="contact" spy={true} smooth={true}><u>Contact</u></Link>
     </div>
   )
 }
