9b3e8603cd79081829cdd8c3010e7502b80c356f
proiacm <proiacm@gmail.com>
2020-12-17 12:35:04 -0700

    Add shadow image

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Hero.css';
 import drone from '../Images/Drone.png';
+import shadow from '../Images/Shadow.png';
 
 const Hero = () => {
 
   return (
     <div className='Hero'>
       <img className='hero-img' src={drone}/>
+      <img className='img-shadow' src={shadow}/>
     </div>
   )
 }
