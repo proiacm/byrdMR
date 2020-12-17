3b9f04ece87048e9e8a437ba577bb08469f7b5ea
proiacm <proiacm@gmail.com>
2020-12-17 13:49:46 -0700

    Create form

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,8 +4,18 @@ import './Contact.css';
 const Contact = () => {
 
   return (
-    <div>
-      Contact
+    <div className='Contact'>
+      <div className='form'>
+        <label for='name'>NAME</label><br/>
+          <input className='input'type="text" id="name" name="name" 
+          placeholder="Enter your full name"/><br/><br/>
+        <label for='email'>EMAIL</label><br/>
+          <input className='input' type="text" id="email" name="email" 
+            placeholder="Enter your email"/><br/><br/>
+        <label for='subject'>MESSAGE</label><br/>
+          <input className='input' type="text" id="subject" name="subject" placeholder="What are your plans?"/><br/>
+          <input className='submit' type="submit" value="Send Message &#8594;"/>
+      </div>
     </div>
   )
 }
