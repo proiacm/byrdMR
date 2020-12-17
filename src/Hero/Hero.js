31ca5a533615cf2e825024b010d89776ebb4e587
proiacm <proiacm@gmail.com>
2020-12-17 12:16:13 -0700

    Add Drone image

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Hero.css';
+import drone from '../Images/Drone.png';
 
 const Hero = () => {
 
   return (
-    <div>
-      Hero
+    <div className='Hero'>
+      <img className='hero-img' src={drone}/>
     </div>
   )
 }
